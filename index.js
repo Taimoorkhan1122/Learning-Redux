aa70861f400c1483390f8cd7f8ede685fd4ae0b6
taimoorkhan1122 <khant8k83@gmail.com>
2021-10-05 15:50:51 +0500

    add multiple reducers

--- index.js
+++ index.js
@@ -2,21 +2,48 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const Buy_Cake = "Buy_Cake";
+const Buy_IceCream = "Buy_IceCream";
 
 function buyCake() {
   return {
     type: Buy_Cake,
-    info: "first redux action",
+    info: "buy cake action creator",
   };
 }
 
+function BuyIceCream() {
+    return {
+        type: Buy_IceCream,
+        info: "buy ice crea action creator"
+    }
+}
+
 // application state should be stored in a single state object tree
-const initialState = {
+// const initialState = {
+//   noOfCakes: 10,
+// };
+const initialCakeState = {
   noOfCakes: 10,
 };
 
+const initialIceCreamState = {
+  noOfIceCreams: 10,
+};
+
 // Reducers: Reducers are responsible for altering the state based on provided actions
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case Buy_Cake:
+//       return {
+//         ...state,
+//         noOfCakes: state.noOfCakes - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case Buy_Cake:
       return {
@@ -28,8 +55,20 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case Buy_IceCream:
+      return {
+        ...state,
+        noOfIceCreams: state.noOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
 // implementing redux store
-const store = createStore(reducer);
+const store = createStore(cakeReducer);
 console.log(store.getState());
 store.subscribe(() => console.log("State Updated: ", store.getState()));
 
