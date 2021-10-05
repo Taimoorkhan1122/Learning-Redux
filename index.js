ac539aa044470af21e56f8637a8749e6173e0fcb
taimoorkhan1122 <khant8k83@gmail.com>
2021-10-05 15:18:12 +0500

    add initial state and Reducer function

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const Buy_Cake  = "Buy_Cake"
+const Buy_Cake = "Buy_Cake";
 
 function buyCake() {
-    return {
-        type: Buy_Cake,
-        info: "first redux action"
-    }
-}  
+  return {
+    type: Buy_Cake,
+    info: "first redux action",
+  };
+}
+
+// application state should be stored in a single state object tree
+const initialState = {
+  noOfCakes: 10,
+};
+
+// Reducers: Reducers are responsible for altering the state based on provided actions
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case Buy_Cake:
+      return {
+        ...state,
+        noOfCakes: state.noOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
