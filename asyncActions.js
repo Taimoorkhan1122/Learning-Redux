535c7514b735297bbb5453ca6186c6d463e2e146
taimoorkhan1122 <khant8k83@gmail.com>
2021-10-10 00:00:23 +0500

    add async action creators

---
+++ asyncActions.js
@@ -0,0 +1,63 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
+const initialState = {
+  loading: false,
+  users: [],
+  errors: "",
+};
+
+// actions
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+// action creators
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        errors: "",
+      };
+
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        errors: action.payload,
+      };
+  }
+};
+
+const store = createStore(reducer);
+
